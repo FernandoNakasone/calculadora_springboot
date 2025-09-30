ce50f253c0342abb66965ac53124eaa96e07b63a
FernandoNakasone <fernandonakasone040@gmail.com>
2025-09-30 09:44:14 -0300

    Inicio da troca de variaveis de double para BigDecimal com a nova funcionalidade return switch

--- src/main/java/br/fiap/calculadora/controller/CalculadoraController.java
+++ src/main/java/br/fiap/calculadora/controller/CalculadoraController.java
@@ -6,6 +6,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.math.BigDecimal;
+
 
 @Controller
 public class CalculadoraController {
@@ -16,10 +18,19 @@ public class CalculadoraController {
         this.service = service;
     }
 
-    @PostMapping("/calculadora")
+   /* @PostMapping("/calculadora")
     public String calculadora(@RequestParam double num1, @RequestParam double num2, @RequestParam String operador, Model model){
 
         model.addAttribute("resultado", service.calcular(num1,num2,operador));
+
+        return "index";
+    }*/
+
+    @PostMapping("/calculadora")
+    public String calculadora(@RequestParam String num1, @RequestParam String num2, @RequestParam String operador, Model model){
+
+        BigDecimal resultado = service.calcular(num1,num2,operador);
+        model.addAttribute("resultado", resultado);
 
         return "index";
     }
--- src/main/java/br/fiap/calculadora/service/CalculadoraService.java
+++ src/main/java/br/fiap/calculadora/service/CalculadoraService.java
@@ -3,10 +3,12 @@ package br.fiap.calculadora.service;
 
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
+
 @Service
 public class CalculadoraService {
 
-    public double calcular(double num1, double num2, String operador){
+   /* public double calcular(double num1, double num2, String operador){
 
         if(operador.equalsIgnoreCase("+")){
             return num1 + num2;
@@ -17,6 +19,28 @@ public class CalculadoraService {
         } else {
             return num1 / num2;
         }
+
+    }*/
+
+    public BigDecimal calcular(String num1, String num2, String operador){
+
+        BigDecimal n1 = toBigDecimal(num1);
+        BigDecimal n2 = toBigDecimal(num2);
+
+        return switch (operador){
+            case "+" -> n1.add(n2);
+            case "-" -> n1.subtract(n2);
+            case "*" -> n1.multiply(n2);
+            case "/" -> {
+                if(n2.compareTo(BigDecimal.ZERO) == 0){
+                    throw new IllegalArgumentException("O segundo número deve ser diferente de zero '0'");
+                }
+
+                n1.divide(n2);
+            }
+        }
+
+        return n1;
 
     }
 
